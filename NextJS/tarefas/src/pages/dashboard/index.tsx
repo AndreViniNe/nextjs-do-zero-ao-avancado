c2dc3922704758fed6dfc83875378364fee0b4d1
Andre Vinicius <andrevini.neves@gmail.com>
2025-04-03 23:05:45 -0300

    fix: changes on Tarefas+ dashboard route

--- NextJS/tarefas/src/pages/dashboard/index.tsx
+++ NextJS/tarefas/src/pages/dashboard/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import styles from './styles.module.css'
+import { TextArea } from '@/components/TextArea'
+import { GetServerSideProps } from 'next'
+
+import {getSession} from 'next-auth/react'
 
 export default function Dashboard() {
     return (
@@ -8,7 +12,41 @@ export default function Dashboard() {
                 <title>Dashboard</title>
             </Head>
 
-            <h1 className={styles.title}>Página painel</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.contentTitle}>Qual sua tarefa?</h1>
+                        <form action="">
+                            <TextArea placeholder='Digite a sua tarefa...'/>
+
+                            <div className={styles.checkboxArea}>
+                                <input type="checkbox" className={styles.checkbox}
+                                />
+                                <label>Deixar tarefa pública</label>
+                            </div>
+                            <button type='submit' className={styles.button}>Registrar</button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req });
+    // console.log(session);
+
+    if (!session?.user) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {},
+    };
+};
